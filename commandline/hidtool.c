a1e8f87c46a5aa8cee2afd7f8e545669dff19158
Aleksandr Maksymenko <masyaman@gmail.com>
2012-12-12 22:55:00 +0200

    Source formatted

--- commandline/hidtool.c
+++ commandline/hidtool.c
@@ -20,149 +20,155 @@
 
 /* ------------------------------------------------------------------------- */
 
-static char *usbErrorMessage(int errCode)
-{
-static char buffer[80];
+static char *usbErrorMessage(int errCode) {
+	static char buffer[80];
 
-    switch(errCode){
-        case USBOPEN_ERR_ACCESS:      return "Access to device denied";
-        case USBOPEN_ERR_NOTFOUND:    return "The specified device was not found";
-        case USBOPEN_ERR_IO:          return "Communication error with device";
-        default:
-            sprintf(buffer, "Unknown USB error %d", errCode);
-            return buffer;
-    }
-    return NULL;    /* not reached */
+	switch (errCode) {
+	case USBOPEN_ERR_ACCESS:
+		return "Access to device denied";
+	case USBOPEN_ERR_NOTFOUND:
+		return "The specified device was not found";
+	case USBOPEN_ERR_IO:
+		return "Communication error with device";
+	default:
+		sprintf(buffer, "Unknown USB error %d", errCode);
+		return buffer;
+	}
+	return NULL; /* not reached */
 }
 
-static usbDevice_t  *openDevice(void)
-{
-usbDevice_t     *dev = NULL;
-unsigned char   rawVid[2] = {USB_CFG_VENDOR_ID}, rawPid[2] = {USB_CFG_DEVICE_ID};
-char            vendorName[] = {USB_CFG_VENDOR_NAME, 0}, productName[] = {USB_CFG_DEVICE_NAME, 0};
-int             vid = rawVid[0] + 256 * rawVid[1];
-int             pid = rawPid[0] + 256 * rawPid[1];
-int             err;
+double getMillis() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+}
 
-    if((err = usbhidOpenDevice(&dev, vid, vendorName, pid, productName, 0)) != 0){
-        fprintf(stderr, "error finding %s: %s\n", productName, usbErrorMessage(err));
-        return NULL;
-    }
-    return dev;
+static usbDevice_t *openDevice(void) {
+	usbDevice_t *dev = NULL;
+	unsigned char rawVid[2] = { USB_CFG_VENDOR_ID }, rawPid[2] = {
+			USB_CFG_DEVICE_ID };
+	char vendorName[] = { USB_CFG_VENDOR_NAME, 0 }, productName[] = {
+			USB_CFG_DEVICE_NAME, 0 };
+	int vid = rawVid[0] + 256 * rawVid[1];
+	int pid = rawPid[0] + 256 * rawPid[1];
+	int err;
+
+	if ((err = usbhidOpenDevice(&dev, vid, vendorName, pid, productName, 0))
+			!= 0) {
+		fprintf(stderr, "error finding %s: %s\n", productName,
+				usbErrorMessage(err));
+		return NULL;
+	}
+	return dev;
 }
 
 /* ------------------------------------------------------------------------- */
 
-static void hexdump(char *buffer, int len)
-{
-int     i;
-FILE    *fp = stdout;
+static void hexdump(char *buffer, int len) {
+	int i;
+	FILE *fp = stdout;
 
-    for(i = 0; i < len; i++){
-        if(i != 0){
-            if(i % 16 == 0){
-                fprintf(fp, "\n");
-            }else{
-                fprintf(fp, " ");
-            }
-        }
-        fprintf(fp, "0x%02x", buffer[i] & 0xff);
-    }
-    if(i != 0)
-        fprintf(fp, "\n");
+	for (i = 0; i < len; i++) {
+		if (i != 0) {
+			if (i % 16 == 0) {
+				fprintf(fp, "\n");
+			} else {
+				fprintf(fp, " ");
+			}
+		}
+		fprintf(fp, "0x%02x", buffer[i] & 0xff);
+	}
+	if (i != 0)
+		fprintf(fp, "\n");
 }
 
-static void int16dump(char *buffer, int len)
-{
-	int     i;
-	FILE    *fp = stdout;
+static void int16dump(char *buffer, int len) {
+	int i;
+	FILE *fp = stdout;
 	len >>= 1;
 	unsigned short *data = buffer;
-    for(i = 0; i < len; i++){
-        if(i != 0){
-            if(i % 16 == 0){
-                fprintf(fp, "\n");
-            }else{
-                fprintf(fp, " ");
-            }
-        }
-        fprintf(fp, "%6d", data[i]);
-    }
-    if(i != 0)
-        fprintf(fp, "\n");
+	for (i = 0; i < len; i++) {
+		if (i != 0) {
+			if (i % 16 == 0) {
+				fprintf(fp, "\n");
+			} else {
+				fprintf(fp, " ");
+			}
+		}
+		fprintf(fp, "%6d", data[i]);
+	}
+	if (i != 0)
+		fprintf(fp, "\n");
 }
 
-static int  hexread(char *buffer, char *string, int buflen)
-{
-char    *s;
-int     pos = 0;
+static int hexread(char *buffer, char *string, int buflen) {
+	char *s;
+	int pos = 0;
 
-    while((s = strtok(string, ", ")) != NULL && pos < buflen){
-        string = NULL;
-        buffer[pos++] = (char)strtol(s, NULL, 0);
-    }
-    return pos;
+	while ((s = strtok(string, ", ")) != NULL && pos < buflen) {
+		string = NULL;
+		buffer[pos++] = (char) strtol(s, NULL, 0);
+	}
+	return pos;
 }
 
-static int  int16read(char *buffer, char *string, int buflen)
-{
-	char    *s;
-	int     pos = 0;
+static int int16read(char *buffer, char *string, int buflen) {
+	char *s;
+	int pos = 0;
 
 	unsigned short *data = buffer;
 
-    while((s = strtok(string, ", ")) != NULL && pos < buflen){
-        string = NULL;
-        *data = (unsigned short)strtol(s, NULL, 0);
-        data++;
-        pos += 2;
-    }
-    return pos;
+	while ((s = strtok(string, ", ")) != NULL && pos < buflen) {
+		string = NULL;
+		*data = (unsigned short) strtol(s, NULL, 0);
+		data++;
+		pos += 2;
+	}
+	return pos;
 }
 
 /* ------------------------------------------------------------------------- */
 
-static void usage(char *myName)
-{
-    fprintf(stderr, "usage:\n");
-    fprintf(stderr, "  %s read\n", myName);
-    fprintf(stderr, "  %s write <listofpositions>\n", myName);
-    fprintf(stderr, "hint: to use servos, values should be in range 1217..3500 (for 12MHz clock).\n");
+static void usage(char *myName) {
+	fprintf(stderr, "usage:\n");
+	fprintf(stderr, "  %s read\n", myName);
+	fprintf(stderr, "  %s write <listofpositions>\n", myName);
+	fprintf(stderr,
+			"hint: to use servos, values should be in range 1217..3500 (for 12MHz clock).\n");
 }
 
-int main(int argc, char **argv)
-{
-usbDevice_t *dev;
-char        buffer[REPORT_SIZE];    /* room for dummy report ID */
-int         err;
+int main(int argc, char **argv) {
+	usbDevice_t *dev;
+	char buffer[REPORT_SIZE]; /* room for dummy report ID */
+	int err;
 
-    if(argc < 2){
-        usage(argv[0]);
-        exit(1);
-    }
-    if((dev = openDevice()) == NULL)
-        exit(1);
-    if(strcasecmp(argv[1], "read") == 0){
-        int len = sizeof(buffer);
-        if((err = usbhidGetReport(dev, 0, buffer, &len)) != 0){
-            fprintf(stderr, "error reading data: %s\n", usbErrorMessage(err));
-        }else{
-        	int16dump(buffer + 1, sizeof(buffer) - 1);
-        }
-    }else if(strcasecmp(argv[1], "write") == 0){
-        int i, pos;
-        memset(buffer, 0, sizeof(buffer));
-        for(pos = 1, i = 2; i < argc && pos < sizeof(buffer); i++){
-            pos += int16read(buffer + pos, argv[i], sizeof(buffer) - pos);
-        }
-        if((err = usbhidSetReport(dev, buffer, sizeof(buffer))) != 0)   /* add a dummy report ID */
-            fprintf(stderr, "error writing data: %s\n", usbErrorMessage(err));
-    }else{
-        usage(argv[0]);
-        exit(1);
-    }
-    usbhidCloseDevice(dev);
-    return 0;
+	if (argc < 2) {
+		usage(argv[0]);
+		exit(1);
+	}
+	if ((dev = openDevice()) == NULL)
+		exit(1);
+	if (strcasecmp(argv[1], "read") == 0) {
+		int len = sizeof(buffer);
+		if ((err = usbhidGetReport(dev, 0, buffer, &len)) != 0) {
+			fprintf(stderr, "error reading data: %s\n", usbErrorMessage(err));
+		} else {
+			int16dump(buffer + 1, sizeof(buffer) - 1);
+		}
+	} else if (strcasecmp(argv[1], "write") == 0) {
+		int i, pos;
+		memset(buffer, 0, sizeof(buffer));
+		for (pos = 1, i = 2; i < argc && pos < sizeof(buffer); i++) {
+			pos += int16read(buffer + pos, argv[i], sizeof(buffer) - pos);
+		}
+		if ((err = usbhidSetReport(dev, buffer, sizeof(buffer))) != 0) /* add a dummy report ID */
+			fprintf(stderr, "error writing data: %s\n", usbErrorMessage(err));
+	} else {
+		usage(argv[0]);
+		exit(1);
+	}
+	usbhidCloseDevice(dev);
+	return 0;
 }
 
 /* ------------------------------------------------------------------------- */
